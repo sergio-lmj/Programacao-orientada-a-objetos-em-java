3667f4c2a643a826e6fc045472526274c0a00316
sergio-lmj <74689701+sergio-lmj@users.noreply.github.com>
2021-06-10 00:49:55 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,32 @@
+package br.com.carros.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Carro {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long idCarro;
+    private String nome;
+
+    public long getIdCarro() {
+        return idCarro;
+    }
+
+    public void setIdCarro(long idCarro) {
+        this.idCarro = idCarro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
